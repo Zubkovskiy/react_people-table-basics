8b8fd39a58a51a1cbd2d50552a4f60f346d079a2
Zubkovskiy Bogdan <zubkovvsbogdan@gmail.com>
2024-10-16 14:56:22 +0300

    release

---
+++ src/components/PeopleTable.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Person } from '../types';
+import { PersonInfo } from './PersonInfo';
+
+interface Props {
+  people: Person[];
+}
+
+const TABLE_HEADING = ['Name', 'Sex', 'Born', 'Died', 'Mother', 'Father'];
+
+export const PeopleTable: React.FC<Props> = ({ people }) => {
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          {TABLE_HEADING.map(column => (
+            <th key={column}>{column}</th>
+          ))}
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => (
+          <PersonInfo key={person.slug} person={person} people={people} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
